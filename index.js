ecd89537686540c223ced7b54f8797be559052f1
Brad Cerasani <bradcerasani@gmail.com>
2014-05-17 11:14:32 -0500

    fixed relative paths

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var markdownpdf = require('markdown-pdf');
 
+var paths = {
+	html5bp: 'node_modules/gulp-markdown-pdf/node_modules/markdown-pdf/html5bp'
+}
+
 module.exports = function (options) {
-	console.log(options);
+
+	if (options.cssPath) {
+		var relativePath = path.relative(paths.html5bp, options.cssPath);
+		options.cssPath = relativePath;
+	}
+
 	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
 			this.push(file);
